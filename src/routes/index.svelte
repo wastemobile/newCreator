a813b141c4a8c7a8c9276b0241b3f1a7a9ceaea0
wastemobile <wastemobile@me.com>
2022-02-12 11:17:56 +0800

    install adaptor-static

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 
-<h1 class="text-2xl font-bold text-center text-gray-800">
+<h1 class="text-2xl font-bold text-gray-800">
     newCreator
 </h1>
+
+<article class="prose prose-lg lg:prose-xl dark:prose-invert">
+    <p>測試部署 SvelteKit 到 Cloudflare Pages.</p>
+</article>
